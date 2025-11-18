527044f27833c09e6e6ae0aec7fc28ad1b6f1944
19Kushagra0 <19Kushagra0@gmail.com>
2025-11-19 04:37:08 +0530

    sending data to component

--- src/app/api/og/dynamic/DynamicImageOg.jsx
+++ src/app/api/og/dynamic/DynamicImageOg.jsx
@@ -3,96 +3,25 @@ import React from "react";
 export default function DynamicImageOg({ groupName }) {
   return (
     <div
+      className="dynamic"
       style={{
         width: "1200px",
         height: "630px",
         background: "white",
         display: "flex",
-        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
         fontFamily: "Inter, sans-serif",
-        gap: "0px",
+        fontSize: "100px",
+        fontWeight: 700,
+        color: "#111",
+        border: "2px solid #e5e7eb",
+        borderRadius: "20px",
+        boxSizing: "border-box",
+        textAlign: "center",
       }}
     >
-      <img
-        src="https://expense-tracker-alpha-lyart.vercel.app/images/background/join-group-pattern.svg"
-        alt="Hero Banner"
-        width="100%"
-        height="300px"
-        style={{
-          objectFit: "contain",
-          position: "absolute",
-        }}
-      />
-      <div
-        style={{
-          width: "290px",
-          marginBottom: "20px",
-          backgroundColor: "black",
-
-          color: "white",
-          padding: "30px 30px",
-          borderRadius: "100px",
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "center",
-          justifyContent: "center",
-          marginTop: "30px",
-          marginLeft: "30px",
-          fontSize: "40px",
-          lineHeight: "normal",
-          fontWeight: "400",
-          letterSpacing: "normal",
-          letterSpacing: "0.7px",
-        }}
-      >
-        INVITATION
-      </div>
-
-      <div
-        style={{
-          fontSize: "100px",
-          fontWeight: 700,
-          color: "#111",
-          marginTop: "200px",
-          marginLeft: "35px",
-        }}
-      >
-        {groupName}
-      </div>
-
-      <div
-        style={{
-          fontSize: 24,
-          fontSize: "40px",
-          lineHeight: 26.1,
-          fontWeight: 400,
-          letterSpacing: 0, // "normal" becomes 0 in OG
-          color: "#5f5f5f",
-          fontFamily: "Inter",
-          marginTop: "-430px",
-          marginLeft: "35px",
-        }}
-      >
-        Add, track and split expenses with your friends
-      </div>
-
-      <img
-        src="https://expense-tracker-alpha-lyart.vercel.app/icons/logo.svg"
-        alt="Hero Banner"
-        width={50}
-        height={50}
-        style={{
-          objectFit: "contain",
-          width: "50px",
-          height: "50px",
-          backgroundColor: "black",
-          position: "absolute",
-
-          right: "50px",
-          bottom: "110px",
-          backgroundColor: "transparent",
-        }}
-      />
+      {groupName}
     </div>
   );
 }
--- src/app/myComponents/dynamicImage/DynamicImage.jsx
+++ src/app/myComponents/dynamicImage/DynamicImage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import "@/app/myComponents/dynamicImage/DynamicImage.css";
-export default function DynamicImage() {
-  return <div className="dynamic"></div>;
+export default function DynamicImage({ groupName }) {
+  return <div className="dynamic">{groupName}</div>;
 }
