809a3102474e05ee69791e499ea9b3818de0e6f9
19Kushagra0 <19Kushagra0@gmail.com>
2025-12-03 17:16:30 +0530

    Dev kushagra (#7)
    * created dynamic og image
    
    * c
    
    * s
    
    * sending data to component
    
    * Update route.js
    
    * Update DynamicImageOg.jsx

--- src/app/api/og/dynamic/DynamicImageOg.jsx
+++ src/app/api/og/dynamic/DynamicImageOg.jsx
@@ -1,6 +1,4 @@
-import React from "react";
-
-export default function DynamicImageOg({ groupName }) {
+export default function JoinGroupOg({ groupName }) {
   return (
     <div
       className="dynamic"
@@ -9,19 +7,90 @@ export default function DynamicImageOg({ groupName }) {
         height: "630px",
         background: "white",
         display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontFamily: "Inter, sans-serif",
-        fontSize: "100px",
-        fontWeight: 700,
-        color: "#111",
-        border: "2px solid #e5e7eb",
-        borderRadius: "20px",
-        boxSizing: "border-box",
-        textAlign: "center",
+        flexDirection: "column",
+        gap: "0px",
+        fontFamily: "Inter",
       }}
     >
-      {groupName}
+      <img
+        src="https://expense-tracker-alpha-lyart.vercel.app/images/background/join-group-pattern.svg"
+        alt="Hero Banner"
+        width="100%"
+        height="300px"
+        style={{
+          objectFit: "contain",
+          position: "absolute",
+        }}
+      />
+      <div
+        style={{
+          width: "290px",
+          marginBottom: "20px",
+          backgroundColor: "black",
+
+          color: "white",
+          padding: "10px 18px",
+          borderRadius: "100px",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "34px",
+          marginLeft: "34px",
+          fontSize: "40px",
+          lineHeight: "normal",
+          fontWeight: "500",
+          letterSpacing: "normal",
+          letterSpacing: "0.7px",
+        }}
+      >
+        INVITATION
+      </div>
+
+      <div
+        style={{
+          fontSize: "100px",
+          fontWeight: 600,
+          color: "#111",
+          marginTop: "200px",
+          marginLeft: "34px",
+          paddingTop: "80px",
+        }}
+      >
+        {groupName}
+      </div>
+
+      <div
+        style={{
+          fontSize: 24,
+          fontSize: "40px",
+          // lineHeight: 26.1,
+          fontWeight: 500,
+          letterSpacing: 0,
+          color: "#828282",
+          marginLeft: "34px",
+        }}
+      >
+        Add, track and split expenses with your friends
+      </div>
+
+      <img
+        src="https://expense-tracker-alpha-lyart.vercel.app/icons/logo.svg"
+        alt="Hero Banner"
+        width={50}
+        height={50}
+        style={{
+          objectFit: "contain",
+          width: "50px",
+          height: "50px",
+          backgroundColor: "black",
+          position: "absolute",
+
+          right: "34px",
+          bottom: "34px",
+          backgroundColor: "transparent",
+        }}
+      />
     </div>
   );
 }
